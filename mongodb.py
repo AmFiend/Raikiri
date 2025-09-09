1bed1b58dc23819c33c174d968d4db215237a266
spxlau <buoyspilux@gmail.com>
2025-09-09 10:37:49 +0300

    Update mongodb.py

--- mongodb.py
+++ mongodb.py
@@ -1,26 +1,19 @@
-import traceback
 import pymongo
 
 client = pymongo.MongoClient(
-        "mongodb+srv://Glitch:PETnvMU8X0414oW2@glitch.u5ylwcm.mongodb.net/?retryWrites=true&w=majority&appName=Glitch"
-
+    "mongodb+srv://Glitch:PETnvMU8X0414oW2@glitch.u5ylwcm.mongodb.net/?retryWrites=true&w=majority&appName=Glitch"
 )
-result = str(client)
 
-if "connect=True" in result:
-    try:
-        print("MONGODB CONNECTED SUCCESSFULLY ✅")
-    except:
-        pass
-else:
-    try:
-        print("MONGODB CONNECTION FAILED ❌")
-    except:
-        pass
+try:
+    print("MONGODB CONNECTED SUCCESSFULLY ✅")
+except:
+    print("MONGODB CONNECTION FAILED ❌")
 
 folder = client["MASTER_DATABASE"]
-usersdb = folder.USERSDB
-chats_auth = folder.CHATS_AUTH
-gcdb = folder.GCDB
-sksdb = client["SKS_DATABASE"].SKS
-confdb = client["SKS_DATABASE"].CONF_DATABASE
+usersdb = folder["USERSDB"]
+chats_auth = folder["CHATS_AUTH"]
+gcdb = folder["GCDB"]
+
+sks_folder = client["SKS_DATABASE"]
+sksdb = sks_folder["SKS"]
+confdb = sks_folder["CONF_DATABASE"]
