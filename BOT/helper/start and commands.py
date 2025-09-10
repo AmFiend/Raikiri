f5107da7942525b6a6dcfb5e096afb726ab26cbe
spxlau <buoyspilux@gmail.com>
2025-09-10 03:49:58 +0300

    Update start and commands.py

--- BOT/helper/start and commands.py
+++ BOT/helper/start and commands.py
@@ -24,8 +24,7 @@ async def cmd_start(client, message):
                 [
                     InlineKeyboardButton("📌 Register", callback_data="register"),
                     InlineKeyboardButton("⚙ Commands", callback_data="cmds")
-                    [InlineKeyboardButton("Helper", callback_data="open_helper")
-                ],
+                    [InlineKeyboardButton("Helper", callback_data="open_helper")],
                 [InlineKeyboardButton("❌ Close", callback_data="close")]
             ]
         )
@@ -226,6 +225,7 @@ Braintree Auth 3
             parse_mode=ParseMode.HTML
         )
         
+
 
 
 
