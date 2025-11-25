eebaf6a63de0ba6eb3d5b5d5ae90fc815b51941e
spxlau <buoyspilux@gmail.com>
2025-11-26 00:55:23 +0300

    Update start and commands.py

--- BOT/helper/start and commands.py
+++ BOT/helper/start and commands.py
@@ -81,19 +81,17 @@ async def callback_command_edit(client, chat_id, text, reply_markup):
 @Client.on_message(filters.command("start", [".", "/"]))
 async def cmd_start(Client, message):
     try:
-       text = """<b>
+        text = """<b>
 𝐒𝐏𝐘𝐃𝐄 𝐂𝐇𝐊  ■□□
 </b>"""
-edit = await message.reply_text(text, message.id)
-await asyncio.sleep(0.5)
+        edit = await message.reply_text(text, message.id)
+        await asyncio.sleep(0.5)
 
-       text = """<b>
-
+        text = """<b>
 𝐒𝐏𝐘𝐃𝐄 𝐂𝐇𝐊  ■■■
-</b> """
-# use edit on the message object
-await edit.edit_text(text)
-await asyncio.sleep(0.5)
+</b>"""
+        await edit.edit_text(text)
+        await asyncio.sleep(0.5)
 
         text = f"""
 <b>🌟 Hello <a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a>!</b>
