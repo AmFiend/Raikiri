2c5b7195d90abdae30e08faeb5ed422991a3f318
spxlau <buoyspilux@gmail.com>
2025-12-24 23:24:30 +0300

    Update mongodb.py

--- mongodb.py
+++ mongodb.py
@@ -1,25 +1,19 @@
 import pymongo
-import os
 
-# Use environment variable for security (better than hardcoding)
-MONGO_URI = os.getenv("MONGO_URI")
+client = pymongo.MongoClient(
+    "mongodb+srv://Glitch:PETnvMU8X0414oW2@glitch.u5ylwcm.mongodb.net/?retryWrites=true&w=majority&appName=Glitch"
+)
 
-# MongoDB client (one per bot)
-client = pymongo.MongoClient(MONGO_URI)
+try:
+    print("MONGODB CONNECTED SUCCESSFULLY ✅")
+except:
+    print("MONGODB CONNECTION FAILED ❌")
 
-# MASTER DATABASE
 folder = client["MASTER_DATABASE"]
 usersdb = folder["USERSDB"]
 chats_auth = folder["CHATS_AUTH"]
 gcdb = folder["GCDB"]
 
-# SKS DATABASE
 sks_folder = client["SKS_DATABASE"]
 sksdb = sks_folder["SKS"]
 confdb = sks_folder["CONF_DATABASE"]
-
-# Shopify bot database
-shopifybot_db = client["shopifybot_db"]
-usersites_collection = shopifybot_db["usersites"]
-
-print("MONGODB CONNECTED ✅")
