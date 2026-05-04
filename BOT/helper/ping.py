3d93e9822fe4b11ebf9dcbbc9f96cf47743fd8f4
spxlau <buoyspilux@gmail.com>
2026-05-05 01:07:24 +0300

    Update ping response format and styling

--- BOT/helper/ping.py
+++ BOT/helper/ping.py
@@ -7,17 +7,18 @@ import time
 async def cmd_ping(client, message):
     try:
         start = time.perf_counter()
-        resp  = """<b>
-🤖 Checking 𝐒𝐏𝐘𝐃𝐄 𝐂𝐇𝐊 𝗣𝗶𝗻𝗴...
-        </b>"""
+        resp  = """✦ <b>ꜱᴘʏᴅᴇ ━ ᴄʜᴇᴄᴋɪɴɢ ᴘɪɴɢ...</b> ✦"""
         edit  = await message.reply_text(resp, quote=True)
         end   = time.perf_counter()
         
-        textb = f"""<b>
-🤖 𝗕𝗼𝘁 𝗡𝗮𝗺𝗲: 𝐒𝐏𝐘𝐃𝐄 𝐂𝐇𝐊
-✅ 𝗕𝗼𝘁 𝗦𝘁𝗮𝘁𝘂𝘀: 𝗥𝘂𝗻𝗻𝗶𝗻𝗴
-📶 𝗣𝗶𝗻𝗴: {(end-start)*1000:.2f} 𝗺𝘀
-        </b>"""
+        textb = f"""✧ <b>ꜱᴘʏᴅᴇ ━ ᴘɪɴɢ</b> ✧
+▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰
+
+◈ <b>ʙᴏᴛ :</b> ꜱᴘʏᴅᴇ ᴄʜᴋ
+⟢ <b>ꜱᴛᴀᴛᴜꜱ :</b> ʀᴜɴɴɪɴɢ ✓
+◈ <b>ᴘɪɴɢ :</b> {(end-start)*1000:.2f} ᴍꜱ
+
+━━━━━━━━━━━━━━━━━━━━"""
         await client.edit_message_text(message.chat.id, edit.id, textb)
 
     except Exception:
