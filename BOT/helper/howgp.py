a6a169122df9c1263b6e367d4c3a1a527834bacb
spxlau <buoyspilux@gmail.com>
2026-05-05 00:58:59 +0300

    Update howgp.py

--- BOT/helper/howgp.py
+++ BOT/helper/howgp.py
@@ -5,18 +5,18 @@ from FUNC.defs import *
 @Client.on_message(filters.command("howgp", [".", "/"]))
 async def cmd_howgp(Client, message):
     try:
-        texta = f"""<b>
-TO THIS ADD THIS BOT TO YOUR GROUP -
+        texta = f"""<a href='https://t.me/elitechkbot?start=start'>✧</a> <b>ꜱᴘʏᴅᴇ ━ ɢʀᴏᴜᴘ ꜱᴇᴛᴜᴘ</b> ✧
+━━━━━━━━━━━━━━━━━━━━
 
-Requirements: <b>Your Group Must Have Atleast 100 Members.</b>
+◈ <b>Requirement:</b> Your Group Must Have Atleast 100 Members.
 
-Steps To Get Your Group Authorised:
-➔ Add This Bot To Your Group As Admin .
-➔ Copy Your Group Username or Group Invite Link .
-➔ Knock @Zer0_ke And Give Him The Group Username or Group Invite Link
+◈ <b>Steps To Get Your Group Authorised:</b>
+   ➔ Add This Bot To Your Group As Admin
+   ➔ Copy Your Group Username or Group Invite Link
+   ➔ Knock @pipin_o And Give Him The Group Username or Group Invite Link
 
-Thats It . Now Once He is got online , He will Approved Your Group ✅ .
-</b>"""
+↪ Once He is online, He will Approve Your Group ✓
+━━━━━━━━━━━━━━━━━━━━"""
         await message.reply_text(texta, message.id)
 
     except Exception as e:
