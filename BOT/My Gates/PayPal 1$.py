78487e317d167ae2720b9bc84f6a984047a1f1af
spxlau <buoyspilux@gmail.com>
2026-05-12 16:30:58 +0300

    Update PayPal 1$.py

--- BOT/My Gates/PayPal 1$.py
+++ BOT/My Gates/PayPal 1$.py
@@ -76,17 +76,17 @@ async def stripe_charge_cmd(Client, message):
         
         firstresp = f"""✧ ᴄʜᴇᴄᴋɪɴɢ. ✧
 
-[玄] 𝘾𝘾 -» <code>{fullcc}</code>
-[玄] 𝙂𝙖𝙩𝙚 -» <i>{gateway}</i>
-[玄] 𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚 -» ■□□□"""
+💠 𝘾𝙘-» <code>{fullcc}</code>
+💠 𝙂𝙖𝙩𝙚-» <i>{gateway}</i>
+💠 𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚-» ■□□□"""
         await asyncio.sleep(0.5)
         firstchk = await message.reply_text(firstresp, quote=True)
 
         secondresp = f"""✧ ᴄʜᴇᴄᴋɪɴɢ.. ✧
 
-[玄] 𝘾𝘾 -» <code>{fullcc}</code>
-[玄] 𝙂𝙖𝙩𝙚 -» <i>{gateway}</i>
-[玄] 𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚 -» ■■■□"""
+💠 𝘾𝙘-» <code>{fullcc}</code>
+💠 𝙂𝙖𝙩𝙚-» <i>{gateway}</i>
+💠 𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚-» ■■■□"""
         await asyncio.sleep(0.5)
         secondchk = await Client.edit_message_text(message.chat.id, firstchk.id, secondresp)
 
@@ -98,24 +98,28 @@ async def stripe_charge_cmd(Client, message):
 
         thirdresp = f"""✧ ᴄʜᴇᴄᴋɪɴɢ... ✧
 
-[玄] 𝘾𝘾 -» <code>{fullcc}</code>
-[玄] 𝙂𝙖𝙩𝙚 -» <i>{gateway}</i>
-[玄] 𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚 -» ■■■■"""
+💠 𝘾𝙘-» <code>{fullcc}</code>
+💠 𝙂𝙖𝙩𝙚-» <i>{gateway}</i>
+💠 𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚-» ■■■■"""
         await asyncio.sleep(0.5)
         thirdcheck = await Client.edit_message_text(message.chat.id, secondchk.id, thirdresp)
 
-        finalresp = f"""
-[玄] 𝘾𝘾 -» <code>{fullcc}</code>
-[玄] 𝙎𝙩𝙖𝙩𝙪𝙨 -» {status}
-[玄] 𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚 -» {response}
-[玄] 𝘽𝙞𝙣 -» {brand} — {type_} — {level}
-[玄] 𝘽𝙖𝙣𝙠 -» {bank}
-[玄] 𝘾𝙤𝙪𝙣𝙩𝙧𝙮 -» {country} {flag}
-[玄] 𝙂𝙖𝙩𝙚𝙬𝙖𝙮 -» {gateway}
-[玄] 𝘾𝙝𝙚𝙘𝙠𝙚𝙙 𝙗𝙮 -» <a href=\'tg://user?id={message.from_user.id}\'>{message.from_user.first_name}</a> ↯ {role}
-[玄] 𝙏𝙞𝙢𝙚 -» {time.perf_counter() - start:0.2f}s"""
-        await asyncio.sleep(0.5)
-        await Client.edit_message_text(message.chat.id, thirdcheck.id, finalresp)
+        finalresp = f"""💠 𝘾𝙘-» <code>{fullcc}</code>
+💠 𝙎𝙩𝙖𝙩𝙪𝙨-» {status}
+💠 𝙍𝙚𝙨𝙪𝙡𝙩-» {response} 💎
+<details><summary>════『 INFO 』════</summary>💠 𝘾𝙤𝙪𝙣𝙩𝙧𝙮-» {country} {flag}
+💠 𝘽𝙞𝙣-» {brand}
+_{type_}-{level}
+💠 𝘽𝙖𝙣𝙠-» {bank}</details>
+════『 META 』════
+💠 𝙂𝙖𝙩𝙚𝙬𝙖𝙮 -» {gateway}
+💠 𝙏𝙞𝙢𝙚-» {time.perf_counter() - start:0.2f}s
+💠 𝘾𝙝𝙚𝙘𝙠𝙚𝙙 𝙗𝙮-» <a href=\'tg://user?id={message.from_user.id}\'>{message.from_user.first_name}</a> ↯
+{role}
+════『 OWNER 』════
+      <a href=\'tg://user?id=8340881349\'>S⊶P⊶I⊶D⊶E⊶R</a>"""
+        
+        await Client.edit_message_text(message.chat.id, thirdcheck.id, finalresp, disable_web_page_preview=True)
         
         await setantispamtime(user_id)
         await deductcredit(user_id)
@@ -217,10 +221,10 @@ async def process_sequential_check(Client, message, ccs, user_id, first_name, ro
     initial_resp = f"""✧ <b>ꜱᴘʏᴅᴇ ━ ᴍᴀꜱꜱ ᴄʜᴇᴄᴋ</b> ✧
 ▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰
 
-[玄] 𝙂𝙖𝙩𝙚 -» PayPal $1
-[玄] 𝘾𝘾 𝘼𝙢𝙤𝙪𝙣𝙩 -» {len(ccs)}
-[玄] 𝘾𝙝𝙚𝙘𝙠𝙞𝙣𝙜 -» {first_name}
-[玄] 𝙎𝙩𝙖𝙩𝙪𝙨 -» Processing...
+💠 𝙂𝙖𝙩𝙚 -» PayPal $1
+💠 𝘾𝘾 𝘼𝙢𝙤𝙪𝙣𝙩 -» {len(ccs)}
+💠 𝘾𝙝𝙚𝙘𝙠𝙞𝙣𝙜 -» {first_name}
+💠 𝙎𝙩𝙖𝙩𝙪𝙨 -» Processing...
 ━━━━━━━━━━━━━━━━━━━━"""
     progress_msg = await message.reply(initial_resp, quote=True)
     header_text = f"""✧ <b>ꜱᴘʏᴅᴇ ━ ᴍᴀꜱꜱ ᴄʜᴇᴄᴋ</b> ✧
@@ -236,13 +240,13 @@ async def process_sequential_check(Client, message, ccs, user_id, first_name, ro
         getbin = await get_bin_details(cc_num)
         brand, type_, level, bank, country, flag = getbin[0], getbin[1], getbin[2], getbin[3], getbin[4], getbin[5]
         
-        card_resp = f"""[玄] 𝘾𝘾 -» <code>{fullcc}</code>
-[玄] 𝙎𝙩𝙖𝙩𝙪𝙨 -» {status}
-[玄] 𝙍𝙚𝙨𝙪𝙡𝙩 -» {response}
-
-[玄] 𝘽𝙞𝙣 -» {brand} — {type_} — {level}
-[玄] 𝘽𝙖𝙣𝙠 -» {bank}
-[玄] 𝘾𝙤𝙪𝙣𝙩𝙧𝙮 -» {country} {flag}
+        card_resp = f"""💠 𝘾𝙘-» <code>{fullcc}</code>
+💠 𝙎𝙩𝙖𝙩𝙪𝙨-» {status}
+💠 𝙍𝙚𝙨𝙪𝙡𝙩-» {response} 💎
+<details><summary>════『 INFO 』════</summary>💠 𝘾𝙤𝙪𝙣𝙩𝙧𝙮-» {country} {flag}
+💠 𝘽𝙞𝙣-» {brand}
+_{type_}-{level}
+💠 𝘽𝙖𝙣𝙠-» {bank}</details>
 ━━━━━━━━━━━━━━━━━━━━
 """
         final_text += card_resp
@@ -255,10 +259,13 @@ async def process_sequential_check(Client, message, ccs, user_id, first_name, ro
         await asyncio.sleep(0.5)
 
     elapsed_time = round(time.perf_counter() - start_time, 2)
-    footer = f"""[玄] 𝙏𝙞𝙢𝙚 -» {elapsed_time}s
-[玄] 𝘾𝙝𝙚𝙘𝙠𝙚𝙙 𝙗𝙮 -» <a href=\'tg://user?id={user_id}\'>{first_name}</a> ↯ {role}
-[玄] 𝙊𝙬𝙣𝙚𝙧 -» @spid_3r
-━━━━━━━━━━━━━━━━━━━━"""
+    footer = f"""════『 META 』════
+💠 𝙂𝙖𝙩𝙚𝙬𝙖𝙮 -» {gateway}
+💠 𝙏𝙞𝙢𝙚-» {elapsed_time}s
+💠 𝘾𝙝𝙚𝙘𝙠𝙚𝙙 𝙗𝙮-» <a href=\'tg://user?id={user_id}\'>{first_name}</a> ↯
+{role}
+════『 OWNER 』════
+      <a href=\'tg://user?id=8340881349\'>S⊶P⊶I⊶D⊶E⊶R</a>"""
     
     final_text += footer
     await progress_msg.edit_text(final_text, disable_web_page_preview=True)
